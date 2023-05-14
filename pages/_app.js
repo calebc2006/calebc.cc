b64df7d2af8b68dd7eefbc79620e39f1544cb931
calebc2006 <calebchia223@gmail.com>
2023-05-14 22:08:05 +0800

    Initial Commit

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+// import '../styles/globals.css'
+
+export default function Home() {
+  return (
+    <>
+      <p>There will be something here eventually</p>
+      <p>(when I have time)</p>
+
+      <div>
+          Source Repo: &nbsp;
+          <a
+            href="https://www.github.com/calebc2006/calebc.cc"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+      </div>
+    </>
+    
+  )
+}
