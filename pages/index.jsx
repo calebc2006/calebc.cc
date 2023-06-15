38d9dbb71a1c2e5bc4a8530ff90a0b67ca9ac4ed
calebc2006 <calebchia223@gmail.com>
2023-06-15 12:50:02 +0800

    fixed ' error

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,7 +12,7 @@ export default function Index() {
             
             <section id='name' className="mx-10 mt-10 h-screen relative">
                 <div className="border-b-4 pb-10 min-w-[90%] top-[10%] absolute">
-                    <h1 className="text-5xl font-bold font-sans select-none">Caleb's<br></br>Blog</h1>
+                    <h1 className="text-5xl font-bold font-sans select-none">Caleb&apos;s<br></br>Blog</h1>
                 </div>
             </section>
 
